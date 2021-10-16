f2a0291350955ca2e96930e8c8b34e8e57a6688c
pawel00wypych <pawelwypych4@gmail.com>
2021-10-16 16:40:03 +0200

    zadanie domowe "simpleCalc" zakończone

--- simpleCalc/src/main.cpp
+++ simpleCalc/src/main.cpp
@@ -1,35 +1,46 @@
 #include <iostream>
+#include <cstdlib>
+#include <string.h>
 #include "calc.h"
 
 using namespace std;
+void error();
 
 int main(int argc, char *argv[])
 {
-    int a=1,b=2,h=2,H=2;
+    int tab[argc];
+    for(int i=1;i<argc;i++)
+    {
+        tab[i]=stoi(argv[i]);
+        cout<<tab[i]<<endl;
+    }
     cout << "SIMPLE CALCULATOR" << endl;
-    int l;
-    while(true) {
-        cout << "Wybierz funkcje:" << endl;
-        cout << "1 - ADD" << endl;
-        cout << "2 - SUBTRACT" << endl;
-        cout << "3 - VOLUME" << endl;
-        cout << "4 - HELP" << endl;
-        cout<<"0 - exit"<<endl;
+    cout << "Wybierz funkcje:" << endl;
+    cout << "1 - ADD" << endl;
+    cout << "2 - SUBTRACT" << endl;
+    cout << "3 - VOLUME" << endl;
+    cout << "4 - HELP" << endl;
 
-        cin >> l;
-        switch (l) {
-            case 0:
-                exit(0);
-                break;
+    switch (tab[1]) {
             case 1:
-               cout<< add(a,b)<<endl;
+                if(argc!=4)
+                    error();
+                 cout<< add(tab[2],tab[3])<<endl;
                 break;
-
             case 2:
-                cout<<subtract(a,b)<<endl;
+                if(argc!=4)
+                    error();
+                cout<<subtract(tab[2],tab[3])<<endl;
                 break;
             case 3:
-                cout<<volume(a,b,h,H)<<endl;
+                if(argc!=6)
+                    error();
+                if(tab[2]<0 || tab[3]<0 || tab[4]<0 || tab[5]<0)
+                {
+                    cout<<"Dlugosc nie moze byc ujemna!\n";
+                    exit(0);
+                }
+                cout<<volume(tab[2],tab[3],tab[4],tab[5])<<endl;
                 break;
             case 4:
                 help();
@@ -38,5 +49,11 @@ int main(int argc, char *argv[])
                 cout << "Zly wybor!" << endl;
                 break;
         }
-    }
+    return 0;
+}
+
+void error()
+{
+    cout<<"\nbledna ilosc argumentow dla tej operacji!\n";
+    exit(1);
 }
