80341bd60444317b12d69278e98e74828fa970f4
pawel00wypych <pawelwypych4@gmail.com>
2021-10-20 13:54:34 +0200

    FristLib zrobiona

---
+++ FirstLib/src/lib.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"lib.h"
+
+using namespace std;
+
+void itIsWork()
+{
+    cout<<"Yes it works!\n";
+}
---
+++ FirstLibCall/main.cpp
@@ -0,0 +1,9 @@
+#include "lib.h"
+
+using namespace std;
+
+int main()
+{
+    itIsWork();
+    return 0;
+}
