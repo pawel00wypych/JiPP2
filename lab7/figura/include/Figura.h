26ac07e085a8128f28e56a21b1713f453e1f4b8d
pawel00wypych <pawelwypych4@gmail.com>
2021-11-27 11:03:38 +0100

    zadania domowe lab7

--- lab7/figura/include/Figura.h
+++ lab7/figura/include/Figura.h
@@ -1,27 +1,18 @@
 #include <iostream>
+#include <math.h>
 
 #ifndef FIRSTLIBCALL_FIGURA_H
 #define FIRSTLIBCALL_FIGURA_H
 
 using namespace std;
 
-class PoleObw {
-protected:
-    double pole,obwod;
-public:
-    PoleObw(){};
-    PoleObw(double pole,double obwod): pole(pole),obwod(obwod){};
-    printP(){cout<<"pole="<<pole<<"\n";};
-    printO(){cout<<"obwod="<<obwod<<"\n";};
-    ~PoleObw(){};
-};
-
 class Figura {
 protected:
     string kolor,nazwa;
+
 public:
     Figura(){};
-    Figura(string kolor,string nazwa):kolor(kolor),nazwa(nazwa){};
+    Figura(string kolor,string nazwa):kolor("zielony"),nazwa("kwadrat"){};
     ~Figura(){};
 };
 
@@ -30,10 +21,49 @@ protected:
     double bok;
 public:
     Kwadrat(){};
-    Kwadrat(double bok):bok(bok){};
-    void printBok(){cout<<"bok="<<bok<<"\n";};
+    Kwadrat(double bok):Figura(),bok(bok){};
+    double obwod()
+    {return 4*bok;}
+    double pole()
+    {return bok*bok;}
     ~Kwadrat(){};
 };
 
+class Trojkat: public Figura {
+protected:
+    double a,b,c;
+public:
+    Trojkat(){};
+    Trojkat(double a,double b,double c):Figura(),a(a),b(b),c(c){
+        double max;
+        max=a;
+        if(max<b)max=b;
+        if(max<c)max=c;
+        if(max==a)
+        {
+            if(a>=c+b)
+            {cout<<"boki nie spelniaja warunku trojkata!\n";exit(1);}
+        }
+        if(max==b)
+        {
+            if(b>=a+c)
+            {cout<<"boki nie spelniaja warunku trojkata!\n";exit(1);}
+        }
+        if(max==c)
+        {
+            if(c>=a+b)
+            {cout<<"boki nie spelniaja warunku trojkata!\n";exit(1);}
+        }
+    };
+    double obwod()
+    {return a+b+c;}
+    double pole()
+    {
+        double p;
+        p=0.5*(a+b+c);
+        return sqrt(p*(p-a)*(p-b)*(p-c));
+    }
+    ~Trojkat(){};
+};
 
 #endif //FIRSTLIBCALL_FIGURA_H
--- lab7/figura/src/main.cpp
+++ lab7/figura/src/main.cpp
@@ -5,7 +5,13 @@ using namespace std;
 
 int main()
 {
-
+    Kwadrat kw1(2);
+    Trojkat tr1(3,4,5);
+    cout<<"nazwa: "<<tr1::nazwa;
+    cout<<"pole trojkata: "<<tr1.pole()<<"\n";
+    cout<<"obwod trojkata: "<<tr1.obwod()<<"\n";
+    cout<<"pole kwadratu: "<<kw1.pole()<<"\n";
+    cout<<"obwod kwadratu: "<<kw1.obwod()<<"\n";
 
     return 0;
 }
